0a42cc1da950dd462c2edd51bc7ce19b3f5333d4
Siroshun09 <siro.shun8@gmail.com>
2019-10-14 05:31:55 +0900

    [Remove] 未使用の import

--- src/main/java/com/github/ucchyocean/lc/japanize/IMEConverter.java
+++ src/main/java/com/github/ucchyocean/lc/japanize/IMEConverter.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
