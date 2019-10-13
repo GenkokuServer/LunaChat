d950c1ab9cca2256a2be9b93c07ad7c826f41bf7
Siroshun09 <siro.shun8@gmail.com>
2019-10-14 06:19:24 +0900

    [Change] Javadoc 修正 & コード整形 (ChannelPlayer)

--- src/main/java/com/github/ucchyocean/lc/channel/ChannelPlayer.java
+++ src/main/java/com/github/ucchyocean/lc/channel/ChannelPlayer.java
@@ -5,76 +5,86 @@
  */
 package com.github.ucchyocean.lc.channel;
 
+import com.github.ucchyocean.lc.Utility;
 import org.bukkit.Location;
 import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
-import com.github.ucchyocean.lc.Utility;
-
 /**
  * プレイヤーの抽象クラス
+ *
  * @author ucchy
  */
 public abstract class ChannelPlayer implements Comparable<ChannelPlayer> {
 
     /**
      * オンラインかどうか
+     *
      * @return オンラインかどうか
      */
     public abstract boolean isOnline();
 
     /**
      * プレイヤー名を返す
+     *
      * @return プレイヤー名
      */
     public abstract String getName();
 
     /**
      * プレイヤー表示名を返す
+     *
      * @return プレイヤー表示名
      */
     public abstract String getDisplayName();
 
     /**
      * プレフィックスを返す
+     *
      * @return プレフィックス
      */
     public abstract String getPrefix();
 
     /**
      * サフィックスを返す
+     *
      * @return サフィックス
      */
     public abstract String getSuffix();
 
     /**
      * メッセージを送る
+     *
      * @param message 送るメッセージ
      */
     public abstract void sendMessage(String message);
 
     /**
      * BukkitのPlayerを取得する
+     *
      * @return Player
      */
     public abstract Player getPlayer();
 
     /**
      * 発言者が今いるワールドのワールド名を取得する
+     *
      * @return ワールド名
      */
     public abstract String getWorldName();
 
     /**
      * 発言者が今いる位置を取得する
+     *
      * @return 発言者の位置
      */
     public abstract Location getLocation();
 
     /**
      * 指定されたパーミッションノードの権限を持っているかどうかを取得する
+     *
      * @param node パーミッションノード
      * @return 権限を持っているかどうか
      */
@@ -82,6 +92,7 @@ public abstract class ChannelPlayer implements Comparable<ChannelPlayer> {
 
     /**
      * 指定されたパーミッションノードが定義されているかどうかを取得する
+     *
      * @param node パーミッションノード
      * @return 定義を持っているかどうか
      */
@@ -89,13 +100,15 @@ public abstract class ChannelPlayer implements Comparable<ChannelPlayer> {
 
     /**
      * 指定されたCommandSenderと同一かどうかを返す
-     * @param sender
+     *
+     * @param sender CommandSender
      * @return 同一かどうか
      */
     public abstract boolean equals(CommandSender sender);
 
     /**
      * 文字列表現を返す
+     *
      * @return 名前管理なら名前、UUID管理なら "$" + UUID を返す
      */
     @Override
@@ -103,12 +116,13 @@ public abstract class ChannelPlayer implements Comparable<ChannelPlayer> {
 
     /**
      * 同一のオブジェクトかどうかを返す
+     *
      * @param other 他方のオブジェクト
      * @return 同一かどうか
      */
     @Override
     public boolean equals(Object other) {
-        if ( !(other instanceof ChannelPlayer) ) {
+        if (!(other instanceof ChannelPlayer)) {
             return false;
         }
         return this.toString().equals(other.toString());
@@ -117,8 +131,9 @@ public abstract class ChannelPlayer implements Comparable<ChannelPlayer> {
     /**
      * インスタンス同士の比較を行う。このメソッドを実装しておくことで、
      * Java8でのHashMapのキー挿入における高速化が期待できる（らしい）。
-     * @param other
-     * @return
+     *
+     * @param other ChannelPlayer
+     * @return int
      * @see java.lang.Comparable#compareTo(java.lang.Object)
      */
     @Override
@@ -128,18 +143,19 @@ public abstract class ChannelPlayer implements Comparable<ChannelPlayer> {
 
     /**
      * 名前またはUUIDから、ChannelPlayerを作成して返す
+     *
      * @param nameOrUuid 名前、または、"$" + UUID
      * @return ChannelPlayer
      */
     public static ChannelPlayer getChannelPlayer(String nameOrUuid) {
-        if ( nameOrUuid.startsWith("$") ) {
+        if (nameOrUuid.startsWith("$")) {
             String id = nameOrUuid.substring(1);
             return new ChannelPlayerUUID(id);
         }
-        if ( Utility.isCB178orLater() ) {
+        if (Utility.isCB178orLater()) {
             ChannelPlayer player =
                     ChannelPlayerUUID.getChannelPlayerUUIDFromName(nameOrUuid);
-            if ( player != null ) {
+            if (player != null) {
                 return player;
             }
         }
@@ -148,17 +164,18 @@ public abstract class ChannelPlayer implements Comparable<ChannelPlayer> {
 
     /**
      * CommandSenderから、ChannelPlayerを作成して返す
-     * @param sender
+     *
+     * @param sender CommandSender
      * @return ChannelPlayer
      */
     public static ChannelPlayer getChannelPlayer(CommandSender sender) {
-        if ( sender == null ) {
+        if (sender == null) {
             return null;
-        } else if ( sender instanceof BlockCommandSender ) {
-            return new ChannelPlayerBlock((BlockCommandSender)sender);
-        } else if ( sender instanceof ConsoleCommandSender ) {
-            return new ChannelPlayerConsole((ConsoleCommandSender)sender);
-        } else if ( Utility.isCB178orLater() ) {
+        } else if (sender instanceof BlockCommandSender) {
+            return new ChannelPlayerBlock((BlockCommandSender) sender);
+        } else if (sender instanceof ConsoleCommandSender) {
+            return new ChannelPlayerConsole((ConsoleCommandSender) sender);
+        } else if (Utility.isCB178orLater()) {
             return ChannelPlayerUUID.getChannelPlayer(sender);
         }
         return new ChannelPlayerName(sender.getName());
