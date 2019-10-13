deca7c5472433f10fd74841dbb0e33d67878f80f
Siroshun09 <siro.shun8@gmail.com>
2019-10-14 06:21:58 +0900

    [Change] Javadoc 修正 & コード整形 (ChannelPlayerBlock)

--- src/main/java/com/github/ucchyocean/lc/channel/ChannelPlayerBlock.java
+++ src/main/java/com/github/ucchyocean/lc/channel/ChannelPlayerBlock.java
@@ -12,6 +12,7 @@ import org.bukkit.entity.Player;
 
 /**
  * コマンドブロック
+ *
  * @author ucchy
  */
 public class ChannelPlayerBlock extends ChannelPlayer {
@@ -20,6 +21,7 @@ public class ChannelPlayerBlock extends ChannelPlayer {
 
     /**
      * コンストラクタ
+     *
      * @param sender コマンドブロック
      */
     ChannelPlayerBlock(BlockCommandSender sender) {
@@ -28,6 +30,7 @@ public class ChannelPlayerBlock extends ChannelPlayer {
 
     /**
      * オンラインかどうか
+     *
      * @return 常にtrue
      * @see com.github.ucchyocean.lc.channel.ChannelPlayer#isOnline()
      */
@@ -38,6 +41,7 @@ public class ChannelPlayerBlock extends ChannelPlayer {
 
     /**
      * プレイヤー名を返す
+     *
      * @return プレイヤー名
      * @see com.github.ucchyocean.lc.channel.ChannelPlayer#getName()
      */
@@ -48,6 +52,7 @@ public class ChannelPlayerBlock extends ChannelPlayer {
 
     /**
      * プレイヤー表示名を返す
+     *
      * @return プレイヤー表示名
      * @see com.github.ucchyocean.lc.channel.ChannelPlayer#getDisplayName()
      */
@@ -58,6 +63,7 @@ public class ChannelPlayerBlock extends ChannelPlayer {
 
     /**
      * プレフィックスを返す
+     *
      * @return 常に空文字列
      * @see com.github.ucchyocean.lc.channel.ChannelPlayer#getPrefix()
      */
@@ -68,6 +74,7 @@ public class ChannelPlayerBlock extends ChannelPlayer {
 
     /**
      * サフィックスを返す
+     *
      * @return 常に空文字列
      * @see com.github.ucchyocean.lc.channel.ChannelPlayer#getSuffix()
      */
@@ -78,7 +85,8 @@ public class ChannelPlayerBlock extends ChannelPlayer {
 
     /**
      * メッセージを送る、実際は何もせずにメッセージを捨てる
-     * @param message
+     *
+     * @param message メッセージ
      * @see com.github.ucchyocean.lc.channel.ChannelPlayer#sendMessage(java.lang.String)
      */
     @Override
@@ -88,6 +96,7 @@ public class ChannelPlayerBlock extends ChannelPlayer {
 
     /**
      * BukkitのPlayerを取得する
+     *
      * @return 常にnullが返される
      * @see com.github.ucchyocean.lc.channel.ChannelPlayer#getPlayer()
      */
@@ -98,6 +107,7 @@ public class ChannelPlayerBlock extends ChannelPlayer {
 
     /**
      * 発言者が今いるワールドのワールド名を取得する
+     *
      * @return 常に "-" が返される。
      * @see com.github.ucchyocean.lc.channel.ChannelPlayer#getWorldName()
      */
@@ -108,12 +118,13 @@ public class ChannelPlayerBlock extends ChannelPlayer {
 
     /**
      * 発言者が今いる位置を取得する
+     *
      * @return 発言者の位置
      * @see com.github.ucchyocean.lc.channel.ChannelPlayer#getLocation()
      */
     @Override
     public Location getLocation() {
-        if ( sender != null ) {
+        if (sender != null) {
             return sender.getBlock().getLocation();
         }
         return null;
@@ -121,6 +132,7 @@ public class ChannelPlayerBlock extends ChannelPlayer {
 
     /**
      * 指定されたパーミッションノードの権限を持っているかどうかを取得する
+     *
      * @param node パーミッションノード
      * @return 権限を持っているかどうか
      * @see com.github.ucchyocean.lc.channel.ChannelPlayer#hasPermission(java.lang.String)
@@ -132,6 +144,7 @@ public class ChannelPlayerBlock extends ChannelPlayer {
 
     /**
      * 指定されたパーミッションノードが定義されているかどうかを取得する
+     *
      * @param node パーミッションノード
      * @return 定義を持っているかどうか
      * @see com.github.ucchyocean.lc.channel.ChannelPlayer#isPermissionSet(java.lang.String)
@@ -143,9 +156,10 @@ public class ChannelPlayerBlock extends ChannelPlayer {
 
     /**
      * 指定されたCommandSenderと同一かどうかを返す
-     * @param sender
+     *
+     * @param sender CommandSender
      * @return 同一かどうか
-     * @see com.github.ucchyocean.lc.channel.ChannelPlayer#equals(org.bukkit.entity.Player)
+     * @see com.github.ucchyocean.lc.channel.ChannelPlayer#equals(Object other)
      */
     @Override
     public boolean equals(CommandSender sender) {
@@ -154,8 +168,9 @@ public class ChannelPlayerBlock extends ChannelPlayer {
 
     /**
      * IDを返す
+     *
      * @return 名前をそのまま返す
-     * @see com.github.ucchyocean.lc.channel.ChannelPlayer#getID()
+     * @see com.github.ucchyocean.lc.channel.ChannelPlayer#getName()
      */
     @Override
     public String toString() {
